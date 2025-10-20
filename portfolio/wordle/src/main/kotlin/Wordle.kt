b7201439f660782879b6e22473d800fb64d1d146
eFrench772 <jpvs7426@leeds.ac.uk>
2025-10-20 16:41:15 +0000

    starting wordle

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,15 @@
 // Implement the six required functions here
+import java.io.File
+
+
+fun main() {
+    // test main not acc gonna be used
+    val words = readWordList("wordle/data/words.txt")
+
+    println("Loaded ${words.size} words")
+    println(words.take(10))
+ }
+
+fun readWordList(filename: String): MutableList<String> {
+   return File(filename).readLines().toMutableList()
+}
