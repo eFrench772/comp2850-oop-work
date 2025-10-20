fceda93a77f219a4085adf637c2f23f5692f58b7
eFrench772 <jpvs7426@leeds.ac.uk>
2025-10-20 18:55:09 +0000

    program that works using main and wordle.kt now we need to create the testing

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,28 @@
+import kotlin.system.exitProcess
+
+
 fun main() {
+   // test main not acc gonna be used
+   val words = readWordList("data/words.txt") //make words.txt into list
+   val chosen = pickRandomWord(words) //pick random word and remove from list then save word
+   val valid = isValid(chosen)
+
+
+   println("Chosen word: $chosen")
+   println("Words left: ${words.size}")
+   println(valid)
+
+
+   println("you have 10 tries to guess the word")
+   for (i in 1..10) {
+    var guess = obtainGuess(i)
+    var matchList = evaluateGuess(guess, chosen)
+    displayGuess(guess, matchList)
+    if (guess == chosen) {
+      println("well done you got the correct word ${chosen} in ${i} guesses ")
+      exitProcess(0)
+    }
+   }
+   println("Oh no! you ran out of guesses the word was ${chosen}")
+   exitProcess(0)
 }
--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,15 +1,61 @@
 // Implement the six required functions here
 import java.io.File
-
-
-fun main() {
-    // test main not acc gonna be used
-    val words = readWordList("wordle/data/words.txt")
-
-    println("Loaded ${words.size} words")
-    println(words.take(10))
- }
+import kotlin.system.exitProcess
 
 fun readWordList(filename: String): MutableList<String> {
    return File(filename).readLines().toMutableList()
+}
+
+fun pickRandomWord(words: MutableList<String>): String {
+   val randomWord = words.random()  // Pick one at random
+   words.remove(randomWord)         // Remove it from the list
+   return randomWord 
+}
+
+fun isValid(word: String): Boolean {
+   if (word.length == 5) {
+      return true
+   } else {
+      return false
+   }
+}
+
+fun obtainGuess(attempt: Int): String {
+   println("Attempt $attempt:")
+   var guess = readln()
+   guess = guess.uppercase()
+   if (guess.length == 5){
+      return guess
+   } else {
+      println("your guess was invalid try again")
+      return obtainGuess(attempt)
+   }
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int> {
+   val guessletters = guess.toList()
+   val targetLetters = target.toList()
+   var check = mutableListOf<Int>()
+
+   for (i in 0..4) {
+      if (guessletters[i] == targetLetters[i]) {
+         check.add(1)
+      } else {
+         check.add(0)
+      }
+   }
+   return check
+}
+
+fun displayGuess(guess: String, matches: List<Int>) {
+   val guessletters = guess.toList()
+   var presentedList = mutableListOf<Char>()
+   for (i in 0..4) {
+      if (matches[i] == 1){
+         presentedList.add(guessletters[i])
+      } else {
+         presentedList.add('?')
+      }
+   }
+   println(presentedList)
 }
